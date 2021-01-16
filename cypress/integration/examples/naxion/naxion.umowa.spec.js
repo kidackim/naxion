c80a2c61dda0e7492c02a3ffb655319a88552e7a
Mariusz Kidacki <it98@g2a.com>
2021-01-16 18:12:02 +0100

    Add some files

--- cypress/integration/examples/naxion/naxion.umowa.spec.js
+++ cypress/integration/examples/naxion/naxion.umowa.spec.js
@@ -1,25 +1,61 @@
 /// <reference types="cypress" />
+import "cypress-file-upload";
+import '@4tw/cypress-drag-drop'
 
-const { canvasToBlob } = require("blob-util")
+context("Navigation", () => {
+  before(() => {
+    cy.visit("https://s1.naxiom.com/");
+  });
 
-context('Navigation', () => {
-  beforeEach(() => {
-    cy.visit('https://s1.naxiom.com/')
-  })
+  Cypress.Commands.add("dragTo", { prevSubject: "element" }, (subject, targetEl) => {
+    cy.wrap(subject).trigger("dragstart");
+    cy.get(targetEl).trigger("drop");
+  }
+);
 
-  it('Login to application and create document', () => {
-    cy.get("#Input_UserName").type("admin-llcd")
-    cy.get('#Input_Password').type('!Q2w3e4r')
-    cy.get("#btn-login").click()
-    cy.get(".sidebar-nav").contains("Umowy").click()
-    cy.get(".k-grid-toolbar").contains("Dodaj umowę").click()
-    cy.get(".d-flex > :nth-child(1)").find("input").type("Umowa{enter}")
-    cy.get(".d-flex > :nth-child(2)").find("input").type("Wladim Jakuszew")
-    cy.get(".d-flex > :nth-child(3)").find("input").type("(Katedra Administracji i Prawa Administracyjnego) Katedra Administracji i Prawa Administracyjnego{enter}")
-    cy.get(".d-flex > :nth-child(4)").find("input").type("(1L) Studia I stopnia (licencjackie)")
-    cy.get(".d-flex > :nth-child(6)").find("input").type("(1) studia stacjonarne")
-    cy.get("button").contains("Zapisz umowę").click()
-    cy.get("button").contains("Zamknij").click()
-  })
-})
+  it("Dodawanie umowy", () => {
+    
+    cy.get("#Input_UserName").type("admin-llcd");
+    cy.get("#Input_Password").type("!Q2w3e4r");
+    cy.get("#btn-login").click();
+    cy.get(".sidebar-nav > .nav > :nth-child(4)").contains("Umowy").click();
+    
+    cy.get(".k-grid-toolbar").contains("Dodaj umowę").click();
+    cy.get(".d-flex > :nth-child(1)").find("input").type("Umowa");
+    cy.contains("Umowa").click();
 
+    cy.get(".d-flex > :nth-child(2)").find("input").type("Wladim Jakuszew");
+    cy.get(".d-flex > :nth-child(3)")
+      .find("input")
+      .type(
+        "(Katedra Administracji i Prawa Administracyjnego) Katedra Administracji i Prawa Administracyjnego"
+      );
+    cy.contains(
+      "(Katedra Administracji i Prawa Administracyjnego) Katedra Administracji i Prawa Administracyjnego"
+    ).click();
+    
+    cy.get(".d-flex > :nth-child(4)")
+      .find("input")
+      .type("(1L) Studia I stopnia (licencjackie)");
+    cy.get(".d-flex > :nth-child(6)")
+      .find("input")
+      .type("(1) studia stacjonarne");
+    cy.get(".card-header-strong > .card-body").contains("Zapisz umowę").click();
+
+    cy.fixture("file.txt").then((fileContent) => {
+      cy.get(".ngx-file-drop__file-input").attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "file.txt",
+        mimeType: "text/txt",
+      });
+    });
+
+    cy.get(".card-header-strong > .card-body").contains("Zapisz umowę").click();
+    cy.get(".card-header-strong > .card-body").contains("Zamknij").click();
+  });
+
+  it("Sprawdź wniosek", () => {
+    cy.get(".sidebar-nav > .nav > :nth-child(2)").contains("Wnioski").click();
+    cy.get(".k-grid-toolbar").contains("Nowy dokument").click(); 
+  });
+});
